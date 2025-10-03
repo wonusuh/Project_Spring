512405056c833824a4414f386a22c44ce6342ad2
Wonu Suh <wonwooseo23@gmail.com>
2025-10-03 13:54:53 +0900

    p. 230

--- sp1/src/main/java/org/zerock/controller/BoardController.java
+++ sp1/src/main/java/org/zerock/controller/BoardController.java
@@ -1,10 +1,15 @@
 package org.zerock.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.dto.BoardDTO;
+import org.zerock.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -14,11 +19,13 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
+	private final BoardService boardService;
 
 	@GetMapping("list")
-	public void list() {
+	public void list(Model model) {
 		log.info("list..................................................................");
 		log.info("board list");
+		model.addAttribute("list", boardService.getList());
 	}
 
 	@GetMapping("register")
@@ -28,30 +35,46 @@ public class BoardController {
 	}
 
 	@PostMapping("register")
-	public String registerPost() {
+	public String registerPost(BoardDTO dto, RedirectAttributes rttr) {
 		log.info("register post..............................................................");
 		log.info("board register post");
+		Long bno = boardService.register(dto);
+		rttr.addFlashAttribute("result", bno);
 		return "redirect:/board/list";
 	}
 
 	@GetMapping("read/{bno}")
-	public String read() {
+	public String read(@PathVariable("bno") Long bno, Model model) {
 		log.info("read..............................................................");
 		log.info("board read");
+		BoardDTO dto = boardService.read(bno);
+		model.addAttribute("board", dto);
 		return "/board/read";
 	}
 
 	@GetMapping("modify/{bno}")
-	public String modifyGET(@PathVariable("bno") Long bno) {
+	public String modifyGET(@PathVariable("bno") Long bno, Model model) {
 		log.info("modify..............................................................");
 		log.info("board modify get");
+		BoardDTO dto = boardService.read(bno);
+		model.addAttribute("board", dto);
 		return "/board/modify";
 	}
 
 	@PostMapping("modify")
-	public String modifyPOST() {
+	public String modifyPOST(BoardDTO boardDTO) {
 		log.info("modify post..............................................................");
 		log.info("board modify post");
-		return "redirect:/board/read/123";
+		boardService.modify(boardDTO);
+		return "redirect:/board/read/" + boardDTO.getBno();
+	}
+
+	@PostMapping("remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove post..............................................................");
+		log.info("board remove post");
+		boardService.remove(bno);
+		rttr.addFlashAttribute("result", bno);
+		return "redirect:/board/list";
 	}
 }
