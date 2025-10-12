42678b2e1afad6d00a19ef8f5704cca81f5d9669
Wonu Suh <wonwooseo23@gmail.com>
2025-10-12 15:15:41 +0900

    p. 371

---
+++ sp1/src/main/java/org/zerock/controller/ProductController.java
@@ -0,0 +1,123 @@
+package org.zerock.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.dto.ProductDTO;
+
+import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
+
+@Controller
+@RequestMapping("/product")
+@Log4j2
+public class ProductController {
+// 나중에 서비스 주입이 필요함
+
+	@GetMapping("register")
+	public void registerGET() {
+		log.info("product register");
+	}
+
+	@PostMapping("register")
+	public String register(ProductDTO productDTO, @RequestParam("files") MultipartFile[] files,
+			RedirectAttributes reArt) {
+		log.info("--------------------------------------------------------");
+		log.info(productDTO);
+		log.info(files);
+
+		// upload files
+		List<String> uploadNames = uploadFiles(files);
+		uploadNames.forEach((eachName) -> {
+			String uuid = eachName.substring(0, 36);
+			String fileName = eachName.substring(37);
+			log.info(uuid);
+			log.info(fileName);
+			productDTO.addImage(uuid, fileName);
+		});
+
+		return "redirect:/product/list";
+	}
+
+	private List<String> uploadFiles(MultipartFile[] files) throws RuntimeException {
+		List<String> uploadNames = new ArrayList<>();
+
+		//
+		if (files == null || files.length == 0) {
+			return uploadNames;
+		}
+
+		String uploadPath = "C:\\upload";
+		log.info(
+				"--------------------------------------------------------------uploadPath--------------------------------------------------------------");
+		log.info(uploadPath);
+
+		for (MultipartFile eachFile : files) {
+			if (eachFile.isEmpty()) {
+				continue;
+			}
+
+			String fileName = eachFile.getOriginalFilename();
+			String uploadName = UUID.randomUUID().toString() + "_" + fileName;
+			File targetFile = null;
+			targetFile = new File(uploadPath, uploadName);
+
+			try (InputStream fin = eachFile.getInputStream(); OutputStream fos = new FileOutputStream(targetFile);) {
+				log.info(targetFile.getAbsolutePath());
+				FileCopyUtils.copy(fin, fos);
+				uploadNames.add(uploadName);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				throw new RuntimeException(e.getMessage());
+			} finally {
+				//
+			}
+
+			//
+			if (eachFile.getContentType().startsWith("image")) {
+				try {
+					Thumbnails.of(targetFile).size(200, 200).toFile(new File(uploadPath, "s_" + uploadName));
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					//
+				}
+			}
+		}
+
+		return uploadNames;
+	}
+
+	public void deleteFiles(List<String> fileNames) {
+		try {
+			File uploadPath = new File("C:\\upload");
+
+			for (String eachFileName : fileNames) {
+				File targetFile = new File(uploadPath, eachFileName);
+				targetFile.delete();
+
+				// 썸네일 파일도 지우기
+				File targetThumb = new File(uploadPath, "s_" + eachFileName);
+				targetThumb.delete();
+			}
+		} catch (Exception e) {
+			//
+		} finally {
+			//
+		}
+	}
+}
--- sp1/src/main/webapp/WEB-INF/views/sample/쿼리.sql
+++ sp1/src/main/webapp/WEB-INF/views/sample/쿼리.sql
@@ -1,89 +1,18 @@
-SELECT b.bno, title, content, writer, regDate, b.updatedate,  COUNT(r.rno) replyCnt 
-
-
-FROM tbl_board  AS b 
-
-LEFT  OUTER 
-
-JOIN tbl_reply  AS r 
-  ON b.bno = r.bno 
-
-
-WHERE b.delflag =  FALSE 
-
-
-GROUP  BY b.bno 
-
-
-ORDER  BY b.bno  DESC; 
- 
-
-
-CREATE  TABLE tbl_product (
-pno  INT  AUTO_INCREMENT  PRIMARY  KEY, -- 상품 번호(고유 식별자)                       
-pname 
-
-VARCHAR(200)  NOT  NULL, -- 상품명                                              
-pdesc 
-
-VARCHAR(1000)  NOT  NULL, -- 상품설명                                           
-price 
-
-INT  NOT  NULL, -- 상품가격                                                     
-sale BOOLEAN 
-
-DEFAULT  FALSE, -- 판매여부 (false)                                     
-writer 
-
-VARCHAR(100)  NOT  NULL, -- 상품 등록자                                        
-regdate 
-
-DATETIME  DEFAULT  CURRENT_TIMESTAMP, -- 상품 등록일                          
-moddate 
-
-DATETIME  DEFAULT  CURRENT_TIMESTAMP  ON 
-
-UPDATE  CURRENT_TIMESTAMP -- 상품 수정
-); 
- 
-
-
-CREATE  TABLE tbl_product_image (
-ino  INT  AUTO_INCREMENT  PRIMARY  KEY, -- 이미지 번호(고유식별자)                      
-pno 
-
-INT  NOT  NULL, -- 상품번호 (외래키)                                              
-filename 
-
-VARCHAR(300)  NOT  NULL, -- 실제 파일명 또는 저장된 경로                     
-
-
-UUID  CHAR(36)  NOT  NULL, -- 파일명 중복 방지를 위한 UUID                             
-
-
-ORD  INT  DEFAULT 0, -- 이미지 정렬순서                                               
-regdate 
-
-DATETIME  DEFAULT  CURRENT_TIMESTAMP, -- 등록일                               
-
-
-FOREIGN  KEY (pno)  REFERENCES tbl_product(pno)  ON 
-
-DELETE  CASCADE 
-); 
- 
-
-
-CREATE  INDEX idx_product_image_pno  ON tbl_product_image(pno,  ORD); 
- 
-
+SELECT b.bno, title, content, writer, regDate, b.updatedate, COUNT(r.rno) replyCnt
+FROM tbl_board AS b LEFT OUTER JOIN tbl_reply AS r ON b.bno = r.bno
+WHERE b.delflag = FALSE
+GROUP  BY b.bno
+ORDER BY b.bno DESC;
 
 SELECT * FROM tbl_product;
 SELECT * FROM tbl_product_image;
- 
-
 
 SELECT p.pno, pname, pdesc, price, sale, writer, p.regdate, ino, UUID, filename, ORD
-FROM tbl_product AS p LEFT OUTER JOIN tbl_product_image AS pimg
-ON p.pno = pimg.pno
+FROM tbl_product AS p LEFT OUTER JOIN tbl_product_image AS pimg ON p.pno = pimg.pno
 WHERE p.pno = 1;
+
+SELECT p.pno, pname, pdesc, price, sale, writer, p.regdate, ino, UUID, filename, ORD
+FROM tbl_product AS p LEFT OUTER JOIN tbl_product_image AS pimg ON p.pno = pimg.pno
+WHERE pimg.ord = 0
+ORDER BY p.pno DESC
+LIMIT 10 OFFSET 0;
