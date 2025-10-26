7fc3cbeaf576492c6651127926225c487669f88f
Wonu Suh <wonwooseo23@gmail.com>
2025-10-26 11:25:32 +0900

    p. 418

--- sp1/src/main/java/org/zerock/security/SecurityConfig.java
+++ sp1/src/main/java/org/zerock/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.extern.log4j.Log4j2;
@@ -15,6 +17,16 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		log.info("---------------------------security config------------------------------");
+
+		http.formLogin((config) -> {
+
+		});
+
 		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
