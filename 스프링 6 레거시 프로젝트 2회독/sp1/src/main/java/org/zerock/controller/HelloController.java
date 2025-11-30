3848ea1e6994dbb9a3ee0fdc7dc57d2eadad2693
서원우 <wonwooseo23@gmail.com>
2025-11-30 14:29:07 +0900

    p. 78

---
+++ 스프링 6 레거시 프로젝트 2회독/sp1/src/main/java/org/zerock/controller/HelloController.java
@@ -0,0 +1,14 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.zerock.service.HelloService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Controller
+@RequiredArgsConstructor
+@ToString
+public class HelloController {
+    private final HelloService helloService;
+}
