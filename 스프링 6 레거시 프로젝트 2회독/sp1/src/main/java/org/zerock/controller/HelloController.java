a237a8a56ec86041a17c438d1a17678e37f87ab5
서원우 <wonwooseo23@gmail.com>
2025-12-06 15:48:31 +0900

    p. 88

--- 스프링 6 레거시 프로젝트 2회독/sp1/src/main/java/org/zerock/controller/HelloController.java
+++ 스프링 6 레거시 프로젝트 2회독/sp1/src/main/java/org/zerock/controller/HelloController.java
@@ -1,14 +1,24 @@
 package org.zerock.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.service.HelloService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
 
 @Controller
 @RequiredArgsConstructor
 @ToString
+@Log4j2
+@RequestMapping("/sample")
 public class HelloController {
     private final HelloService helloService;
+
+    @GetMapping("/ex1")
+    public void ex1() {
+	log.info("/sample/ex1");
+    }
 }
