cc6d4f4c2a85a91776acdf2d115f981cfd2aa574
서원우 <wonwooseo23@gmail.com>
2026-01-25 04:30:10 +0900

    p. 300

---
+++ 스프링 6 레거시 프로젝트 2회독/sp1/src/main/java/org/zerock/controller/ReplyController.java
@@ -0,0 +1,28 @@
+package org.zerock.controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.dto.ReplyDTO;
+import org.zerock.service.ReplyService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/replies")
+public class ReplyController {
+    private final ReplyService replyService;
+
+    @PostMapping("")
+    public ResponseEntity<Map<String, Long>> add(ReplyDTO replyDTO) {
+	log.info(replyDTO);
+	replyService.add(replyDTO);
+	return ResponseEntity.ok(Map.of("result", replyDTO.getRno()));
+    }
+}
