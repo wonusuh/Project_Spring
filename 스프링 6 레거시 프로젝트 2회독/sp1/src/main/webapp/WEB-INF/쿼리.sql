cdf4d0886a4735d23012781fa3651bf8475cf35c
서원우 <wonwooseo23@gmail.com>
2026-05-16 15:17:35 +0900

    상품 테이블 생성

---
+++ 스프링 6 레거시 프로젝트 2회독/sp1/src/main/webapp/WEB-INF/쿼리.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tbl_product (
+	-- 상품번호
+	pno INT AUTO_INCREMENT PRIMARY KEY,
+	-- 상품 이름
+	pname VARCHAR(200) NOT NULL,
+	-- 상품 설명
+	pdesc VARCHAR(200) NOT NULL,
+	-- 상품 가격
+	price INT NOT NULL,
+	-- 판매 여부(false)
+	sale BOOLEAN DEFAULT FALSE,
+	-- 상품 등록자
+	writer VARCHAR(100) NOT NULL,
+	regdate DATETIME DEFAULT CURRENT_TIMESTAMP,
+	moddate DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+SELECT * FROM tbl_product;
