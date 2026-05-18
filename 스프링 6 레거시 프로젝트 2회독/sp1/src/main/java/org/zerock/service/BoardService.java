be92bafdcf9310964bb0a70d8c4bd597707ce2e2
서원우 <wonwooseo23@gmail.com>
2026-05-18 18:09:39 +0900

    코드 포매팅만

--- 스프링 6 레거시 프로젝트 2회독/sp1/src/main/java/org/zerock/service/BoardService.java
+++ 스프링 6 레거시 프로젝트 2회독/sp1/src/main/java/org/zerock/service/BoardService.java
@@ -17,46 +17,43 @@ import lombok.extern.log4j.Log4j2;
 @Transactional
 public class BoardService {
 
-	private final BoardMapper boardMapper;
+    private final BoardMapper boardMapper;
 
-	
-	public List<BoardDTO> getList() {
-		
-		
-		return boardMapper.list();
-	}
-	
-	public Long register(BoardDTO dto) {
-		
-		
-		int insertCount = boardMapper.insert(dto);
-		
-		log.info("insertCount: " + insertCount);
-		
-		return dto.getBno();
-		
-	}
+    public List<BoardDTO> getList() {
 
-	public BoardDTO read(Long bno) {
-		
-		BoardDTO boardDTO = boardMapper.selectOne(bno);
-		
-		return boardDTO;
-		
-	}
-	
-	public void remove(Long bno) {
-		
-		boardMapper.remove(bno);
-		
-	}
-	
-	public void modify(BoardDTO boardDTO) {
-		
-		boardMapper.update(boardDTO);
-		
-	}
-	
+	return boardMapper.list();
+    }
+
+    public Long register(BoardDTO dto) {
+
+	int insertCount = boardMapper.insert(dto);
+
+	log.info("insertCount: " + insertCount);
+
+	return dto.getBno();
+
+    }
+
+    public BoardDTO read(Long bno) {
+
+	BoardDTO boardDTO = boardMapper.selectOne(bno);
+
+	return boardDTO;
+
+    }
+
+    public void remove(Long bno) {
+
+	boardMapper.remove(bno);
+
+    }
+
+    public void modify(BoardDTO boardDTO) {
+
+	boardMapper.update(boardDTO);
+
+    }
+
 //	public BoardListPagingDTO getList(int page, int size) {
 //		
 //		//페이지 번호가 0보다 작으면 무조건 1페이지
@@ -73,32 +70,23 @@ public class BoardService {
 //		return new BoardListPagingDTO(list, total, page, size);
 //		
 //	}
-	
-	public BoardListPagingDTO getList(int page, int size, String typeStr, String keyword) {
-		
-		//페이지 번호가 0보다 작으면 무조건 1페이지
-		page = page <= 0? 1 : page;
-		//사이즈가 10보다 작거나 100보다 크면 10
-		size = (size <= 10 || page > 100) ? 10: size;
-		
-		int skip = (page -1 ) * size;
-		
-		String[] types = typeStr != null ? typeStr.split(""): null;
-		
-		
-		List<BoardDTO> list = boardMapper.listSearch(skip, size, types, keyword);
-		
-		int total = boardMapper.listCountSearch(types, keyword);
-		
-		return new BoardListPagingDTO(list, total, page, size, typeStr, keyword);
-		
-	}
 
-	
+    public BoardListPagingDTO getList(int page, int size, String typeStr, String keyword) {
 
+	// 페이지 번호가 0보다 작으면 무조건 1페이지
+	page = page <= 0 ? 1 : page;
+	// 사이즈가 10보다 작거나 100보다 크면 10
+	size = (size <= 10 || page > 100) ? 10 : size;
 
+	int skip = (page - 1) * size;
 
+	String[] types = typeStr != null ? typeStr.split("") : null;
 
+	List<BoardDTO> list = boardMapper.listSearch(skip, size, types, keyword);
 
+	int total = boardMapper.listCountSearch(types, keyword);
 
+	return new BoardListPagingDTO(list, total, page, size, typeStr, keyword);
+
+    }
 }
